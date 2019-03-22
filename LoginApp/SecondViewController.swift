19b48d09b39333ea3a1cc6ad5701afa48e84c0f6
allanshivji <allanmshj@gmail.com>
2019-03-22 10:26:02 -0400

    Added all files

---
+++ LoginApp/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  LoginApp
+//
+//  Created by Allan Shivji on 3/21/19.
+//  Copyright © 2019 Allan Shivji. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
+    
+    let mycontext:LAContext = LAContext()
 
+    @IBOutlet weak var loginButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if mycontext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil) {
+            if mycontext.biometryType == .faceID {
+                loginButton.setImage(UIImage(named: "faceID"), for: UIControl.State.normal)
+            } else if mycontext.biometryType == .touchID {
+                loginButton.setImage(UIImage(named: "fingerID"), for: UIControl.State.normal)
+            }
+        }
+        
     }
 
-
+    @IBAction func loginAction(_ sender: Any) {
+    
+        if mycontext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil) {
+            
+            mycontext.evaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Your Account") { (correct, nil) in
+                
+                if correct {
+                    
+                    DispatchQueue.main.async {
+                        self.performSegue(withIdentifier: "SecondViewController", sender: self)
+                    }
+                    
+                } else {
+                    print("Invalid Password")
+                }
+            }
+        } else {
+            print("Does not support Biometric")
+        }
+        
+    }
+    
 }
 
